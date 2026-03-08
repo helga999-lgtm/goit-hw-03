e149cfdae92cf852e758ddf4d34c82da6dec8ebb
Olha Tyshchenko <ol91ha@gmail.com>
2026-03-08 17:40:44 +0200

    Fix lottery numbers function

---
+++ task_2.py
@@ -0,0 +1,15 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+
+    if min < 1 or max > 1000 or quantity > (max - min + 1):
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+
+    return sorted(numbers)
+
+
+lottery_numbers = get_numbers_ticket(1, 1000, 6)
+print(lottery_numbers)
+
